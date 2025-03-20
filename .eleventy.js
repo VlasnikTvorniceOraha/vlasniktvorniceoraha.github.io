21518eb014e572bf920b56407b0756922fe1d8b5
VlasnikTvorniceOraha <jaksa.fajs@gmail.com>
2025-03-20 17:32:44 +0100

    file type change for displaying images properly

--- .eleventy.js
+++ .eleventy.js
@@ -14,8 +14,7 @@ module.exports = async function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("source/js");
     eleventyConfig.addPassthroughCopy("source/images");
     eleventyConfig.addPassthroughCopy("source/projectMedia");
-    eleventyConfig.addPassthroughCopy("node_modules/flickity/dist");
-    eleventyConfig.addPassthroughCopy("node_modules/leaflet/dist");
+    
 
     eleventyConfig.addCollection("sortedProjects", function (collection) {
         return collection.getFilteredByTag("project").sort((a, b) => {
